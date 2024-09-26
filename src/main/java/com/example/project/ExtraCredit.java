0d3f8417bfd8cbbb1e04396438a1a667cac19dfc
FatinIshraq4 <fatini5@nycstudents.net>
2024-09-26 21:53:03 +0000

    hi

--- src/main/java/com/example/project/ExtraCredit.java
+++ src/main/java/com/example/project/ExtraCredit.java
@@ -7,8 +7,24 @@ public class ExtraCredit {
 
     public static String calculateTip(int people, int percent, double cost, String items) {
         StringBuilder result = new StringBuilder();
+        double tip = (((double) percent) / 100) * cost;
+        double totalBill = tip + cost;
+        double costBeforeTip = cost / people;
+        double tipPerPerson = tip / people;
+        double totalCostPerPerson = totalBill / people;
 
-                //COPY AND PASTE YOUR CODE HERE 
+
+        // Learned how to round with string.format from https://www.javatpoint.com/two-decimal-places-java
+
+        result.append("-------------------------------\n");
+        result.append("Total bill before tip: $" + cost + "\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: " + percent + "%" + "\n");
+        result.append("Total tip: $" +  String.format("%.2f", tip) + "\n");
+        result.append("Total Bill with tip: $" +  String.format("%.2f", totalBill) + "\n");
+        result.append("Per person cost before tip: $" +  String.format("%.2f", costBeforeTip) + "\n");
+        result.append("Tip per person: $" +  String.format("%.2f", tipPerPerson) + "\n");
+        result.append("Total cost per person: $" +  String.format("%.2f", totalCostPerPerson) + "\n");
+        result.append("-------------------------------\n"); 
 
         //the two lines  should go below result.append("-------------------------------\n"); 
         result.append("Items ordered:\n");
@@ -19,13 +35,23 @@ public class ExtraCredit {
     }
                                    
     public static void main(String[] args) {
-        int people;
-        int percent;
-        double cost;
-        String items; 
+        int people = 6;
+        int percent = 25;
+        double cost = 52.27;
+        String items = ""; 
 
-        //Your scanner object and while loop should go here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an item name or type -1 to finish:");
+        String itemName = scan.nextLine();
+        StringBuilder itemList = new StringBuilder(items);
+        
+        while (!itemName.equals("-1")) {
+            itemList.append(itemName).append("\n");
+            System.out.println("Enter an item name or type -1 to finish:");
+            itemName = scan.nextLine();
+        }
+
                              
-        System.out.println(calculateTip(people,percent,cost,items));
+        System.out.println(calculateTip(people,percent,cost,itemList.toString()));
     }
 }
